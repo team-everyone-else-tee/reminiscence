91ac50ed60ce5daa42eea0837fd136275ff834df
Merry Cimakasky <mkcimakasky@gmail.com>
2019-05-13 09:52:42 -0700

    set up

---
+++ reminiscence/src/main/java/com/reminiscence/reminiscence/ReminiscenceApplication.java
@@ -0,0 +1,13 @@
+package com.reminiscence.reminiscence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminiscenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReminiscenceApplication.class, args);
+	}
+
+}
