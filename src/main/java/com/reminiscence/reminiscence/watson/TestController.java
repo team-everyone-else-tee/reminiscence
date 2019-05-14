9fd632cb78cf5f4ffcbc3734966c958d7ebe1941
jaezuchowski <jess.zuchowski@gmail.com>
2019-05-14 13:38:14 -0700

    one watson test passed

---
+++ src/main/java/com/reminiscence/reminiscence/watson/TestController.java
@@ -0,0 +1,31 @@
+package com.reminiscence.reminiscence.watson;
+
+import com.ibm.cloud.sdk.core.service.security.IamOptions;
+import com.ibm.watson.tone_analyzer.v3.ToneAnalyzer;
+import com.ibm.watson.tone_analyzer.v3.model.ToneAnalysis;
+import com.ibm.watson.tone_analyzer.v3.model.ToneOptions;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+    @GetMapping("/watson")
+    public String getTestWatson() {
+
+        IamOptions options = new IamOptions.Builder()
+                .apiKey("6qDP7NDHH7luqX1i5iNvnmnFruMAGosa96WbcCfpEyX9")
+                .build();
+
+        ToneAnalyzer toneAnalyzer = new ToneAnalyzer("2017-09-21", options);
+        toneAnalyzer.setEndPoint("https://gateway.watsonplatform.net/tone-analyzer/api");
+
+        String text = "hey, dad, I won football with the layups";
+
+        ToneOptions toneOptions = new ToneOptions.Builder()
+                .text(text)
+                .build();
+        ToneAnalysis tone = toneAnalyzer.tone(toneOptions).execute().getResult();
+        System.out.println(tone);
+        return "home";
+    }
+}
