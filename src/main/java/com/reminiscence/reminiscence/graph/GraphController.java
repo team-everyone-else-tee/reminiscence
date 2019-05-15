70fe6c14acde5b33319b444938073b1320aad27c
MichaelJahns <jahnsmic001@gmail.com>
2019-05-15 14:47:11 -0700

    Adds data visualization for all entries belonging to the current user

--- src/main/java/com/reminiscence/reminiscence/graph/GraphController.java
+++ src/main/java/com/reminiscence/reminiscence/graph/GraphController.java
@@ -1,21 +1,72 @@
 package com.reminiscence.reminiscence.graph;
 
+import com.reminiscence.reminiscence.account.AccountRepo;
+import com.reminiscence.reminiscence.account.UserAccount;
+import com.reminiscence.reminiscence.journal.Entry;
+import com.reminiscence.reminiscence.watson.Tone;
+import com.reminiscence.reminiscence.watson.ToneRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class GraphController {
 
+    @Autowired
+    AccountRepo accountRepo;
+
+    @Autowired
+    ToneRepo toneRepo;
+
     @GetMapping("/pieGraph")
-    public String pieGraph(Model model) {
-        int anger = 6;
-        int fear = 12;
-        int sadness = 6;
-        int analytical = 7;
-        int confident = 3;
-        int tentative = 6;
-        
+    public String pieGraph(
+            Model model,
+            Principal p
+    ) {
+        //Collect every entry associated with a user
+        UserAccount user = accountRepo.findByUsername(p.getName());
+        List<Entry> journal = user.getJournal();
+        //Collect every tone associated with every entry of a user
+        List<Tone> allTones = new ArrayList<>();
+        for (Entry e : journal) {
+            allTones.addAll(e.getTones());
+        }
+
+        //Initialize every possible tone
+        int anger = 0;
+        int fear = 0;
+        int sadness = 0;
+        int analytical = 0;
+        int confident = 0;
+        int tentative = 0;
+
+        for (Tone tone : allTones) {
+            switch (tone.getTone()) {
+                case "Anger":
+                    anger++;
+                    break;
+                case "Fear":
+                    fear++;
+                    break;
+                case "Sadness":
+                    sadness++;
+                    break;
+                case "Analytical":
+                    analytical++;
+                    break;
+                case "Confident":
+                    confident++;
+                    break;
+                case "Tentative":
+                    tentative++;
+                    break;
+            }
+        }
         model.addAttribute("anger", anger);
         model.addAttribute("fear", fear);
         model.addAttribute("sadness", sadness);
