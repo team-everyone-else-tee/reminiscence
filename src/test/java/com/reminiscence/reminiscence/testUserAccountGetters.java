0b9b102768f3173da1732fc6f94ea3a9e373cfcd
MichaelJahns <jahnsmic001@gmail.com>
2019-05-16 11:39:43 -0700

    All tests pass UserAccountGetters

--- src/test/java/com/reminiscence/reminiscence/testUserAccountGetters.java
+++ src/test/java/com/reminiscence/reminiscence/testUserAccountGetters.java
@@ -1,6 +1,6 @@
 package com.reminiscence.reminiscence;
 
-import com.reminiscence.reminiscence.account.AccountController;
+import com.reminiscence.reminiscence.account.AccountRepo;
 import com.reminiscence.reminiscence.account.UserAccount;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,6 +13,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -29,8 +31,7 @@ public class testUserAccountGetters {
     private WebApplicationContext webApplicationContext;
 
     @Autowired
-    AccountController accountController;
-
+    AccountRepo accountRepo;
 
     @Autowired
     PasswordEncoder encoder;
@@ -49,23 +50,37 @@ public class testUserAccountGetters {
 
     @Test
     public void testGetUserName() {
-
+        assertEquals("Username was unexpected", "testUser1", user1.getUsername());
+        assertEquals("Username was unexpected", "testUser2", user2.getUsername());
+        assertEquals("Username was unexpected", "testUser3", user3.getUsername());
     }
 
     @Test
     public void testGetPassword() {
+        assertNotEquals("Password was unexpected", "password", user1.getPassword());
+        assertNotEquals("Password was unexpected", "farts", user2.getPassword());
+        assertNotEquals("Password was unexpected", "1234", user3.getPassword());
     }
 
     @Test
     public void testGetJoinDate() {
-    }
-
-    @Test
-    public void testGetId() {
+        assertEquals("Join Date was unexpected", "1994", user1.getJoinDate());
+        assertEquals("Join Date was unexpected", "2017-5-25", user2.getJoinDate());
+        assertEquals("Join Date was unexpected", "0000-1-1", user3.getJoinDate());
     }
 
     @Test
     public void testGetJournal() {
+        assertNull("Journal was unexpectedly not null", user1.getJournal());
     }
+
+    @Test
+    public void testGetId() {
+        UserAccount user4 = new UserAccount("testUser4", "danger", encoder, "2000");
+        user4 = accountRepo.save(user4);
+
+        assertEquals("Id was unexpected", 1, user4.getId());
+    }
+
 
 }
