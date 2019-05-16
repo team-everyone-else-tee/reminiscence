5e543f7645a1254e12c17970e192422befab9fce
jaezuchowski <jess.zuchowski@gmail.com>
2019-05-16 12:17:09 -0700

    tone displayed on single entry view

--- src/main/java/com/reminiscence/reminiscence/watson/ToneRepo.java
+++ src/main/java/com/reminiscence/reminiscence/watson/ToneRepo.java
@@ -3,4 +3,5 @@ package com.reminiscence.reminiscence.watson;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ToneRepo extends JpaRepository<Tone, Long> {
+    public Tone findByEntryId(Long id);
 }
